a7715136f9504fdd093ff17dccda32680dd606a0
casjay <git-admin@casjaysdev.com>
2021-01-01 03:34:12 -0500

    Update plugins.fish

--- plugins.fish
+++ plugins.fish
@@ -1,10 +1,10 @@
 #!/usr/bin/env fish
 
 # Ensure oh-my-fish is installed
-if test ! -f ~/.config/fish/omf-install
-    curl -LSs https://get.oh-my.fish >~/.config/fish/omf-install
-    fish ~/.config/fish/omf-install --noninteractive --yes
-    fish ~/.config/fish/plugins.fish
+if test ! -f "$HOME/.config/fish/omf-install" && ! -d "$HOME/.local/share/fish/oh-my-fish"
+    curl -LSs https://get.oh-my.fish >"$HOME/.config/fish/omf-install"
+    fish "$HOME/.config/fish/omf-install" --noninteractive --yes
+    fish -c "$HOME/.config/fish/plugins.fish"
 end
 
 fish -c "omf install \
