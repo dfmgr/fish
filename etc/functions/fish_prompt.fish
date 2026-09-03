28a2dc482d95ee1426b5434aa298fea3e107aef7
casjay <git-admin@casjaysdev.pro>
2026-09-03 13:28:55 -0400

    🐛 Fix escape codes and word-splitting bug in fish_prompt.fish 🐛
    Fixes real bugs found during the repo-wide fish audit: single-quoted
    escape-code strings aren't interpreted by echo -n in fish, and an
    unquoted command substitution passed to `test -n` word-split on
    multiple untracked files.
    
    - etc/functions/fish_prompt.fish: replace raw single-quoted escape-
      code strings with set_color / set_color --background / set_color
      normal calls
    - etc/functions/fish_prompt.fish: fix test -n (git ls-files ...)
      word-splitting bug by capturing into $git_untracked and quoting it
    - etc/functions/fish_prompt.fish: trim trailing whitespace

--- etc/functions/fish_prompt.fish
+++ etc/functions/fish_prompt.fish
@@ -21,25 +21,25 @@
 function fish_prompt --description 'Custom powerline-style prompt matching bash'
     # Save last exit status
     set -l last_status $status
-    
+
     # Colors matching bash prompt (using bright variants)
-    set -l color_bg_purple '\e[45m'
-    set -l color_bg_red '\e[41m'
-    set -l color_bg_green '\e[42m'
-    set -l color_bg_yellow '\e[43m'
-    set -l color_bg_magenta '\e[45m'
-    set -l color_bg_blue '\e[44m'
-    set -l color_fg_white '\e[97m'
-    set -l color_fg_black '\e[30m'
-    set -l color_reset '\e[0m'
-    
+    set -l color_bg_purple (set_color --background purple)
+    set -l color_bg_red (set_color --background red)
+    set -l color_bg_green (set_color --background green)
+    set -l color_bg_yellow (set_color --background yellow)
+    set -l color_bg_magenta (set_color --background magenta)
+    set -l color_bg_blue (set_color --background blue)
+    set -l color_fg_white (set_color white)
+    set -l color_fg_black (set_color black)
+    set -l color_reset (set_color normal)
+
     # Prompt parts
     set -l prompt_parts
-    
+
     # OS/Shell version icon
     set -l shell_icon "🐧"
     set prompt_parts $prompt_parts "$color_bg_blue$color_fg_white $shell_icon Fish $FISH_VERSION $color_reset"
-    
+
     # PHP version
     if type -q php
         set -l php_ver (php -v 2>/dev/null | head -1 | awk '{print $2}' | cut -d- -f1)
@@ -47,7 +47,7 @@ function fish_prompt --description 'Custom powerline-style prompt matching bash'
             set prompt_parts $prompt_parts "$color_bg_purple$color_fg_white PHP:$php_ver $color_reset"
         end
     end
-    
+
     # Ruby version
     if type -q ruby
         set -l ruby_ver (ruby -v 2>/dev/null | awk '{print $2}')
@@ -55,7 +55,7 @@ function fish_prompt --description 'Custom powerline-style prompt matching bash'
             set prompt_parts $prompt_parts "$color_bg_red$color_fg_white Ruby:$ruby_ver $color_reset"
         end
     end
-    
+
     # Node version
     if type -q node
         set -l node_ver (node -v 2>/dev/null | tr -d 'v')
@@ -63,7 +63,7 @@ function fish_prompt --description 'Custom powerline-style prompt matching bash'
             set prompt_parts $prompt_parts "$color_bg_green$color_fg_black Node:$node_ver $color_reset"
         end
     end
-    
+
     # Python version
     if type -q python3
         set -l python_ver (python3 --version 2>/dev/null | awk '{print $2}')
@@ -76,7 +76,7 @@ function fish_prompt --description 'Custom powerline-style prompt matching bash'
             set prompt_parts $prompt_parts "$color_bg_red$color_fg_white Py:$python_ver $color_reset"
         end
     end
-    
+
     # Rust version
     if type -q rustc
         set -l rust_ver (rustc --version 2>/dev/null | awk '{print $2}')
@@ -84,7 +84,7 @@ function fish_prompt --description 'Custom powerline-style prompt matching bash'
             set prompt_parts $prompt_parts "$color_bg_red$color_fg_black Rust:$rust_ver $color_reset"
         end
     end
-    
+
     # Go version
     if type -q go
         set -l go_ver (go version 2>/dev/null | awk '{print $3}' | tr -d 'go')
@@ -92,12 +92,12 @@ function fish_prompt --description 'Custom powerline-style prompt matching bash'
             set prompt_parts $prompt_parts "$color_bg_yellow$color_fg_black Go:$go_ver $color_reset"
         end
     end
-    
+
     # Git info
     if git rev-parse --git-dir >/dev/null 2>&1
         set -l git_branch (git branch 2>/dev/null | sed -n '/\* /s///p')
         set -l git_status ""
-        
+
         # Check for changes
         if not git diff --quiet 2>/dev/null
             set git_status "$git_status!"
@@ -105,25 +105,26 @@ function fish_prompt --description 'Custom powerline-style prompt matching bash'
         if not git diff --cached --quiet 2>/dev/null
             set git_status "$git_status+"
         end
-        if test -n (git ls-files --others --exclude-standard 2>/dev/null)
+        set -l git_untracked (git ls-files --others --exclude-standard 2>/dev/null)
+        if test -n "$git_untracked"
             set git_status "$git_status?"
         end
-        
+
         if test -n "$git_branch"
             set prompt_parts $prompt_parts "$color_bg_magenta$color_fg_white  $git_branch$git_status $color_reset"
         end
     end
-    
+
     # Current directory
     set -l pwd_display (prompt_pwd)
     set prompt_parts $prompt_parts "$color_bg_blue$color_fg_white  $pwd_display $color_reset"
-    
+
     # Print first line with all parts
     for part in $prompt_parts
         echo -n $part
     end
     echo ""
-    
+
     # Second line: prompt character with exit status
     if test $last_status -eq 0
         echo -n " 😇 "
