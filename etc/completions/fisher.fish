6a2be079794ac484cca8854bbb07d9a23e8177b1
casjay <git-admin@casjaysdev.pro>
2026-09-03 13:57:43 -0400

    🐛 Add missing standard headers to etc/completions/ files 🐛
    - etc/completions/fisher.fish: added the mandated standard file
      header (AI.md § 2.2); file previously had no shebang or header at
      all, inconsistent with every other .fish file in the repo
    - etc/completions/generate.fish: added the mandated standard file
      header below the existing shebang; file previously had only a bare
      shebang with no shellcheck directive or header block

--- etc/completions/fisher.fish
+++ etc/completions/fisher.fish
@@ -1,3 +1,19 @@
+#!/usr/bin/env fish
+# shellcheck shell=fish
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 202609031400-git
+# @Author        : Jason Hempstead
+# @Contact       : jason@casjaysdev.pro
+# @License       : LICENSE.md
+# @ReadME        : fisher --help
+# @Copyright     : Copyright: (c) 2026 Jason Hempstead, CasjaysDev
+# @Created       : Thursday, Sep 03, 2026 14:00 EDT
+# @File          : fisher.fish
+# @Description   : completions for the fisher plugin manager
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 complete --command fisher --exclusive --long help --description "Print help"
 complete --command fisher --exclusive --long version --description "Print version"
 complete --command fisher --exclusive --condition __fish_use_subcommand --arguments install --description "Install plugins"
--- etc/completions/generate.fish
+++ etc/completions/generate.fish
@@ -1,3 +1,17 @@
 #!/usr/bin/env fish
-
+# shellcheck shell=fish
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+##@Version       : 202609031400-git
+# @Author        : Jason Hempstead
+# @Contact       : jason@casjaysdev.pro
+# @License       : LICENSE.md
+# @ReadME        : fuck --help
+# @Copyright     : Copyright: (c) 2026 Jason Hempstead, CasjaysDev
+# @Created       : Thursday, Sep 03, 2026 14:00 EDT
+# @File          : generate.fish
+# @Description   : generates a thefuck completion via make_completion
+# @TODO          :
+# @Other         :
+# @Resource      :
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 make_completion fuck thefuck
